7f69fda8d0be3bff0a7fc4a11d4eed434d35f176
Angel Paruas <paruas@codetel.net.do>
2016-05-06 15:07:55 -0400

    Initial Commit

---
+++ Ejercicio 2.1.6.4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejercicio 2.1.6.4
+//
+//  Created by Angel Paruas on 5/6/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
